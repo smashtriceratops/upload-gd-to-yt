62b4cb0aa0de093edc2b96b10215f7746dbe122c
smashtriceratops <135593217+smashtriceratops@users.noreply.github.com>
2023-09-11 14:02:58 +1000

    Update upload_video.py

--- upload_video.py
+++ upload_video.py
@@ -132,7 +132,9 @@ def resumable_upload(insert_request):
       if response is not None:
         if 'id' in response:
           print ("Video id '%s' was successfully uploaded." % response['id'])
-          response = response
+          file = open("/content/upload-gd-to-yt/response.txt", "w")
+          file.write(response)
+          file.close()
         else:
           exit("The upload failed with an unexpected response: %s" % response)
     except HttpError as e:
